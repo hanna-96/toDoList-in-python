551f0262652a14632ed0beace66286adcca152d0
Hanna Rzheutskaya <a.rzheutskaya96@gmail.com>
2020-11-15 20:27:54 -0500

    redefined List entity

--- lists.py
+++ lists.py
@@ -1,7 +1,7 @@
 def def_lists_entity(db,orm):
     class List(db.Entity):
         name = orm.Required(str)    
-        numberOfCompletedItems = orm.Optional(int)
         totalItems = orm.Required(int)
+        numberOfCompletedItems = orm.Optional(int)
         items = orm.Set('Item')
     return List
